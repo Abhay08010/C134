9e2d42e59f3fb330a617534286d50b2bad0f6f4d
Abhay08010 <110532944+Abhay08010@users.noreply.github.com>
2023-04-03 15:48:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(800,700);
+    canvas.position(850,150);
+    video = createCapture(VIDEO);
+    video.size(800,700)
+}
+
+function draw(){
+
+}
